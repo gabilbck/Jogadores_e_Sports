0360a8b03001fb063f6c0f78c14da77c396b28e3
Laiz Detros <megami.ay@gmail.com>
2024-10-22 15:13:24 -0300

    Delete JogadorInterface.java

--- JogadorInterface.java
+++
@@ -1,7 +0,0 @@
-public interface JogadorInterface{
-    void addJogador(Jogador jogador) throws Exception;      // add  - adicionar jogador
-    Jogador searchJogador(int codigo) throws Exception;     // search - pesquisa jogador
-    void uptJogador(Jogador jogador) throws Exception;      // update - atualizar jogador
-    void delJogador(int codigo) throws Exception;           // delete - deletar jogador
-    List<Jogador> listJogadores() throws Exception;         // list - lista os jogadores
-}
