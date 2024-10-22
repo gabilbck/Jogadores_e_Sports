dcdb2548d2796e0df3b610d8b26750a63e35c010
Laiz Detros <megami.ay@gmail.com>
2024-10-22 19:31:29 -0300

    Update jogadores_e_sports.sql

--- src/jogadores_e_sports.sql
+++ src/jogadores_e_sports.sql
@@ -1,3 +1,14 @@
-CREATE TABLE jogador (id INT NOT NULL, nome VARCHAR(100), salario DOUBLE NOT NULL, experiencia INT NOT NULL, categoria VARCHAR(2), PRIMARY KEY(id));
+CREATE TABLE jogador (
+      id INT NOT NULL, 
+      nome VARCHAR(100), 
+      salario DOUBLE NOT NULL, 
+      experiencia INT NOT NULL, 
+      categoria INT, 
+      PRIMARY KEY(idP), 
+      FOREIGN KEY (categoria) REFERENCES tipo_jogo(id));
 
-CREATE TABLE tipo_jogo (id INT NOT NULL, categoria VARCHAR(2), descricao VARCHAR(100), PRIMARY KEY(id));
+CREATE TABLE tipo_jogo (
+      id INT NOT NULL, 
+      categoria VARCHAR(2), 
+      descricao VARCHAR(100), 
+      PRIMARY KEY(id));
