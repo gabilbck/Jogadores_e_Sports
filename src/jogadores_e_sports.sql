630f8f8e336f2baa0ebfd1bcda6a488f30eaa61d
Laiz Detros <megami.ay@gmail.com>
2024-10-22 16:17:12 -0300

    correção

--- src/jogadores_e_sports.sql
+++ src/jogadores_e_sports.sql
@@ -1,1 +1,1 @@
-CREATE TABLE jogadores (codigo INT PRIMARY KEY, nome VARCHAR(100), salario DOUBLE, anosTreino INT);
+CREATE TABLE jogador (id_jogador int not null, nome_jogador varchar(100), salario_jogador double not null, experiencia_jogador int not null, primary key(id_jogador));
