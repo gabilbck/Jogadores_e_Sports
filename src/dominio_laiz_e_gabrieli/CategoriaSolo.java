2dff221ce728dd0e81f31f70d8beaff1df91f8cf
Laiz Detros <megami.ay@gmail.com>
2024-10-23 09:03:03 -0300

    Update CategoriaSolo.java

--- src/dominio_laiz_e_gabrieli/CategoriaSolo.java
+++ src/dominio_laiz_e_gabrieli/CategoriaSolo.java
@@ -1,1 +1,7 @@
+package dominio_laiz_e_gabrieli;
 
+public class CategoriaSolo extends Categoria {
+    public CategoriaSolo(int idCategoria, String categoria, String descricao) {
+        super(idCategoria, categoria, descricao);
+    }
+}
