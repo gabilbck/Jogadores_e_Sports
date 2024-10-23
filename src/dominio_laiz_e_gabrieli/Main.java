ad6dcaa73d42cca9d5f8bb23285365cc6c0793df
Laiz Detros <megami.ay@gmail.com>
2024-10-23 09:21:30 -0300

    Update Main.java

--- src/dominio_laiz_e_gabrieli/Main.java
+++ src/dominio_laiz_e_gabrieli/Main.java
@@ -20,7 +20,8 @@ public class Main {
                 System.out.println("3. Atualizar Jogador");
                 System.out.println("4. Deletar Jogador");
                 System.out.println("5. Listar Jogadores");
-                System.out.println("6. Sair");
+                System.out.println("6. Listar Categorias");
+                System.out.println("7. Sair");
 
                 int opcao = scanner.nextInt();
                 switch (opcao) {
@@ -34,12 +35,14 @@ public class Main {
                         double salario = scanner.nextDouble();
                         System.out.print("Digite os anos de treino: ");
                         int anosTreino = scanner.nextInt();
+                        System.out.print("Digite a categoria (1 para solo e 2 para grupo): ");
+                        int categoria = scanner.nextInt();
 
                         Jogador jogador;
                         if (anosTreino >= 5) {
-                            jogador = new JogadorVeterano(codigo, nome, salario, anosTreino);
+                            jogador = new JogadorVeterano(codigo, nome, salario, anosTreino, categoria);
                         } else {
-                            jogador = new JogadorTrainee(codigo, nome, salario, anosTreino);
+                            jogador = new JogadorTrainee(codigo, nome, salario, anosTreino, categoria);
                         }
                         jogadorFuncs.addJogador(jogador);
                         System.out.println("Jogador adicionado com sucesso.");
@@ -50,7 +53,7 @@ public class Main {
                         int codigoBusca = scanner.nextInt();
                         Jogador jogadorBuscado = jogadorFuncs.searchJogador(codigoBusca);
                         if (jogadorBuscado != null) {
-                            System.out.println("Jogador encontrado: " + jogadorBuscado.getNomeJogador());
+                            System.out.println("Jogador encontrado: " + jogadorBuscado.getNome());
                         } else {
                             System.out.println("Jogador não encontrado.");
                         }
@@ -58,6 +61,7 @@ public class Main {
 
                     case 3:
                         // Similar à adição, mas com atualização
+                        // fazer depois
                         break;
 
                     case 4:
@@ -70,7 +74,7 @@ public class Main {
                     case 5:
                         List<Jogador> jogadores = jogadorFuncs.listJogadores();
                         for (Jogador j : jogadores) {
-                            System.out.println(j.getNomeJogador() + " - Salário: " + j.calcularSalario());
+                            System.out.println(j.getNome() + " - Salário: " + j.calcularSalario() + " - Categoria Jogada: " + j.getCategoria());
                         }
                         break;
 
