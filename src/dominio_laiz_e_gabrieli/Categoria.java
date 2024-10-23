4425964f77a1aa95b475f481ee27a7cf2174b9a8
Laiz Detros <megami.ay@gmail.com>
2024-10-23 08:57:32 -0300

    Update Categoria.java

--- src/dominio_laiz_e_gabrieli/Categoria.java
+++ src/dominio_laiz_e_gabrieli/Categoria.java
@@ -1,1 +1,32 @@
+package dominio_laiz_e_gabrieli;
 
+public abstract class Jogador {
+	    private int idCategoria;
+	    private String categoria;
+	    private String descricao;
+
+    	// Setters
+  	  public Jogador(int idCategoria, String categoria, String descricao) {
+  		  this.idCategoria = idCategoria;
+  		  this.categoria = categoria;
+  		  this.descricao = descricao;
+  	  }
+
+    	// Getters
+  	  public int getIdCategoria() {
+  		  return idCategoria;
+  	  }
+  	  public String getCategoria() {
+  		  return categoria;
+  	  }
+  	  public String getDesc() {
+  		  return descricao;
+  	  }
+  	
+  	  public int getCategoria() {
+  		  return categoria;
+  	  }
+	
+	  // Metodos
+	  //public abstract double calcular();
+}
