3030e09cb7c2ac9b8d51f252644a9cc3858cce18
Laiz Detros <megami.ay@gmail.com>
2024-10-23 08:52:31 -0300

    Update JogadorTrainee.java

--- src/dominio_laiz_e_gabrieli/JogadorTrainee.java
+++ src/dominio_laiz_e_gabrieli/JogadorTrainee.java
@@ -1,12 +1,12 @@
 package dominio_laiz_e_gabrieli;
 
 public class JogadorTrainee extends Jogador {
-    public JogadorTrainee(int idJogador, String nomeJogador, double salarioJogador, int experienciaJogador) {
-        super(idJogador, nomeJogador, salarioJogador, experienciaJogador);
+    public JogadorTrainee(int idJogador, String nome, double salario, int experiencia, int categoria) {
+        super(idJogador, nome, salario, experiencia, categoria);
     }
 
     @Override
     public double calcularSalario() {
-        return getSalarioJogador();  // Salário fixo
+        return getSalario();  // Salário fixo
     }
 }
