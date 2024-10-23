743e4716a31ed1d5519e371f6ee7ef1d61ef0523
Laiz Detros <megami.ay@gmail.com>
2024-10-23 08:32:44 -0300

    SQLs de inserção dos tipos

--- src/jogadores_e_sports.sql
+++ src/jogadores_e_sports.sql
@@ -1,3 +1,4 @@
+-- Cria a tabela de jogadores
 CREATE TABLE jogador (
       id INT NOT NULL, 
       nome VARCHAR(100), 
@@ -7,8 +8,21 @@ CREATE TABLE jogador (
       PRIMARY KEY(idP), 
       FOREIGN KEY (categoria) REFERENCES tipo_jogo(id));
 
+-- cria a tabela de categoria de jogo que os jogadores vão estar ligados
 CREATE TABLE tipo_jogo (
       id INT NOT NULL, 
       categoria VARCHAR(2), 
       descricao VARCHAR(100), 
       PRIMARY KEY(id));
+
+/*
+criando as duas categorias:
+id->: 1 - para solo
+id-> 2 - para em grupo
+*/
+
+INSERT INTO tipo_jogo (id, categoria, descricao)
+VALUES ("1", "Solo", "Nesta categoria os jogadores jogam de forma individual. Times são proibidos.");
+
+INSERT INTO tipo_jogo (id, categoria, descricao)
+VALUES ("2", "Grupo", "Nesta categoria os jogadores jogam acompanhados.");
