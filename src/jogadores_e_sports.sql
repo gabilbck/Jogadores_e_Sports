9d2e824112ff819ea64036c8c34ea19cdd1377a9
gadulb <gabrielieduardalembeck@gmail.com>
2024-10-24 22:38:29 -0300

    alt sql

--- src/jogadores_e_sports.sql
+++ src/jogadores_e_sports.sql
@@ -1,19 +1,27 @@
--- Cria a tabela de jogadores
-CREATE TABLE jogador (
-      id INT NOT NULL AUTO_INCREMENT, 
-      nome VARCHAR(100), 
-      salario DOUBLE NOT NULL, 
-      experiencia INT NOT NULL, 
-      categoria INT, 
-      PRIMARY KEY(id), 
-      FOREIGN KEY (categoria) REFERENCES categoria(id));
-
--- cria a tabela de categoria de jogo que os jogadores vão estar ligados
 CREATE TABLE categoria (
-      id INT NOT NULL AUTO_INCREMENT, 
-      nome VARCHAR(5), 
-      descricao VARCHAR(100), 
-      PRIMARY KEY(id));
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(255) NOT NULL,
+    descricao VARCHAR(255)
+);
+
+CREATE TABLE equipe (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(255) NOT NULL,
+    qt_Jogadores INT,
+    categoria_id INT,
+    FOREIGN KEY (categoria_id) REFERENCES categoria(id)
+);
+
+CREATE TABLE jogador (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(255) NOT NULL,
+    salario DOUBLE,
+    experiencia INT,
+    equipe_id INT,
+    categoria_id INT,
+    FOREIGN KEY (equipe_id) REFERENCES equipe(id),
+    FOREIGN KEY (categoria_id) REFERENCES categoria(id)
+);
 
 /*
 criando as duas categorias:
@@ -21,11 +29,21 @@ id-> 1 - solo
 id-> 2 - grupo
 */
 
-INSERT INTO categoria_jogo (id, categoria, descricao)
+INSERT INTO categoria (id, categoria, descricao)
 VALUES ("1", "Solo", "Nesta categoria os jogadores jogam de forma individual. Times sao proibidos.");
 
-INSERT INTO categoria_jogo (id, categoria, descricao)
+INSERT INTO categoria (id, categoria, descricao)
 VALUES ("2", "Grupo", "Nesta categoria os jogadores jogam acompanhados.");
+
+/*
+criando duas equipes base/exemplo:
+*/
+
+INSERT INTO equipe (id, nome, qt_jogadores, categoria_id)
+VALUES ("", "Diamond", "5", "2");
+
+INSERT INTO equipe (id, nome, qt_jogadores, categoria_id)
+VALUES ("", "Stars", "6", "2");
 
 /*
 criando dois jogadores base/exemplo:
@@ -33,8 +51,8 @@ treinee que joga na categoria de grupo
 veterno que joga na categoria solo
 */
 -- não é necessário passar um id, pois os campos de PK são Auto_increment
-INSERT INTO jogador(id, nome, salario, experiencia, categoria)
-VALUES ("", "Joao", "3500.00", "2", "2");
+INSERT INTO jogador(id, nome, salario, experiencia, equipe_id, categoria_id)
+VALUES ("", "Joao", "3500.00", "2", "1", "2");
 
-INSERT INTO jogador(id, nome, salario, experiencia, categoria)
-VALUES ("", "Lucas", "9500.00", "6", "1");
+INSERT INTO jogador(id, nome, salario, experiencia, equipe_id, categoria_id)
+VALUES ("", "Lucas", "9500.00", "6", NULL, "1");
