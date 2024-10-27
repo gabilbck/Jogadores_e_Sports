501c90d51b1ea9fb353de72818aca6c8592c7f8c
Laiz Detros <megami.ay@gmail.com>
2024-10-27 14:55:25 -0300

    Update jogadores_e_sports.sql

--- src/jogadores_e_sports.sql
+++ src/jogadores_e_sports.sql
@@ -47,9 +47,9 @@ criando duas equipes base/exemplo:
 */
 
 INSERT INTO equipe (nome, qt_jogadores, categoria_id)
-VALUES ("Sem Equipe", 0, 1) 
-       ("Diamond", 5, 2),
-       ("Stars", 6, 2);
+VALUES (1, "Sem Equipe", 0, 1) 
+       (2, "Diamond", 5, 2),
+       (3, "Stars", 6, 2);
 
 -- ou
 
@@ -68,6 +68,14 @@ treinee que joga na categoria de grupo
 veterno que joga na categoria solo
 */
 -- não é necessário passar um id, pois os campos de PK são Auto_increment
+
+INSERT INTO jogador(id, nome, salario, experiencia, equipe_id, categoria_id)
+VALUES (1, "Joao", 3550.00, 2, 2, 2),
+       (2, "Maria", 9500.00, 6, 1, 1),
+       (3, "Lucas", 4070.00, 4, 3, 2);
+
+-- ou
+
 INSERT INTO jogador(id, nome, salario, experiencia, equipe_id, categoria_id)
 VALUES ("", "Joao", "3500.00", "2", "1", "2");
 
