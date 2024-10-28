3e4123862ec53aeb61e8e185e157a803039001b7
Gabrieli Eduarda Lembeck <79219121+gabilbck@users.noreply.github.com>
2024-10-28 18:09:53 -0300

    Update jogadores_e_sports.sql

--- src/jogadores_e_sports.sql
+++ src/jogadores_e_sports.sql
@@ -7,7 +7,6 @@ CREATE TABLE categoria (
 CREATE TABLE equipe (
     id INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(255) NOT NULL,
-    qt_jogadores INT,
     categoria_id INT,
     FOREIGN KEY (categoria_id) REFERENCES categoria(id)
 );
@@ -15,7 +14,8 @@ CREATE TABLE equipe (
 CREATE TABLE jogador (
     id INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(255) NOT NULL,
-    salario DOUBLE,
+    salario_bruto DOUBLE,
+    salario_total_recebido DOUBLE,
     experiencia INT,
     equipe_id INT,
     categoria_id INT,
@@ -34,33 +34,14 @@ INSERT INTO categoria (id, nome, descricao)
 VALUES (1, "Solo", "Nesta categoria os jogadores jogam de forma individual. Times são proibidos."),
        (2, "Grupo", "Nesta categoria os jogadores jogam acompanhados.");
 
--- ou
-
-INSERT INTO categoria (id, categoria, descricao)
-VALUES ("1", "Solo", "Nesta categoria os jogadores jogam de forma individual. Times sao proibidos.");
-
-INSERT INTO categoria (id, categoria, descricao)
-VALUES ("2", "Grupo", "Nesta categoria os jogadores jogam acompanhados.");
-
 /*
 criando duas equipes base/exemplo:
 */
 
-INSERT INTO equipe (id, nome, qt_jogadores, categoria_id)
-VALUES (1, "Sem Equipe", 0, 1),
-       (2, "Diamond", 5, 2),
-       (3, "Stars", 6, 2);
-
--- ou
-
-INSERT INTO equipe (id, nome, qt_jogadores, categoria_id)
-VALUES ("", "Sem Equipe", "0", "1");
-
-INSERT INTO equipe (id, nome, qt_jogadores, categoria_id)
-VALUES ("", "Diamond", "5", "2");
-
-INSERT INTO equipe (id, nome, qt_jogadores, categoria_id)
-VALUES ("", "Stars", "6", "2");
+INSERT INTO equipe (id, nome, categoria_id)
+VALUES (1, "Sem Equipe", 1),
+       (2, "Diamond", 2),
+       (3, "Stars", 2);
 
 /*
 criando dois jogadores base/exemplo:
@@ -69,15 +50,7 @@ veterno que joga na categoria solo
 */
 -- não é necessário passar um id, pois os campos de PK são Auto_increment
 
-INSERT INTO jogador(id, nome, salario, experiencia, equipe_id, categoria_id)
-VALUES (1, "Joao", 3550.00, 2, 2, 2),
-       (2, "Maria", 9500.00, 6, 1, 1),
-       (3, "Lucas", 4070.00, 4, 3, 2);
-
--- ou
-
-INSERT INTO jogador(id, nome, salario, experiencia, equipe_id, categoria_id)
-VALUES ("", "Joao", "3500.00", "2", "1", "2");
-
-INSERT INTO jogador(id, nome, salario, experiencia, equipe_id, categoria_id)
-VALUES ("", "Lucas", "9500.00", "6", NULL, "1");
+INSERT INTO jogador(id, nome, salario_bruto, salario_total_recebido, experiencia, equipe_id, categoria_id)
+VALUES (1, "Joao", 3550.00, 0, 2, 2, 2),
+       (2, "Maria", 9500.00, 0, 6, 1, 1),
+       (3, "Lucas", 4070.00, 0, 4, 3, 2);
